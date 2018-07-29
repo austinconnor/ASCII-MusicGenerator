f7f0b0413f6d8ab6f60d746d344ffc0ee101ef25
austinconnor <32618204+austinconnor@users.noreply.github.com>
2018-07-28 22:24:18 -0400

    Add files via upload

--- music.py
+++ music.py
@@ -1,9 +1,10 @@
 import clef as c
 import sys
 import key
+import timeSig as ts
 import stringManager
 
-#ACII ART CREDIT: http://www.chris.com/ascii/index.php?art=music/musical%20notation
+#ASCII ART CREDIT: http://www.chris.com/ascii/index.php?art=music/musical%20notation
 
 def main():
     print("Welcome to Connor's ASCII sheet music generator!")
@@ -22,6 +23,10 @@ def main():
     print("Enter a key: (i.e. 'Bb', 'C#', 'A')")
     choice = input("> ")
     master = key.setKey(choice,clef,master) #sets key, adds key signature
+    print(master)
+    print("Enter a time signature: (i.e. '4/4', '3/4', '6/8')")
+    choice = input("> ")
+    master = ts.setTime(choice, master)
     print(master)
 
 if __name__ == '__main__':
---
+++ timeSig.py
@@ -0,0 +1,74 @@
+import string
+import stringManager as sm
+
+beats = 0
+note = 0
+
+def setTime(time, master):
+    if(time == "4/4"):
+        beats = 4
+        note = 4
+        string = ''' 
+      
+      
+      
+--/|--
+ / |  
+/__|--
+   |  
+--/|--
+ / |  
+/__|--
+   |  
+------
+      
+      
+      
+ '''
+
+    if(time == "3/4"):
+        beats = 3
+        note = 4
+        string = ''' 
+      
+      
+      
+-__---
+   )  
+--<---
+ __)  
+--/|--
+ / |  
+/__|--
+   |  
+------
+      
+      
+      
+ '''
+
+    if(time == "2/4"):
+        beats = 2
+        note = 4
+        string = ''' 
+      
+      
+      
+-__---
+   )  
+--/---
+ /__  
+--/|--
+ / |  
+/__|--
+   |  
+------
+      
+      
+      
+ '''
+    return sm.concat(master.splitlines(),string.splitlines())
+
+
+def checkTime():
+    return
